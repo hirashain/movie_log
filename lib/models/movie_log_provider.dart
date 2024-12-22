e3747444d7c536a144aaa475eeb3308381378262
hirashain <eternal.hirashain@gmail.com>
2024-12-23 00:09:10 +0900

    登録した映画とユーザ設定をデータベースに保存できるようにした

--- lib/models/movie_log_provider.dart
+++ lib/models/movie_log_provider.dart
@@ -6,19 +6,47 @@ import 'movie.dart';
 class MovieLogProvider with ChangeNotifier {
   List<Movie> _movies = [];
   int _numColumns = 3;
-  late Database _database;
+  late Database _moviesDatabase;
+  late Database _userSettingsDatabase;
 
   MovieLogProvider() {
-    _initDatabase();
+    _initUserSettingsDatabase();
+    _initMoviesDatabase();
   }
 
   List<Movie> get movies => _movies;
   int get numColumns => _numColumns;
 
-  Future<void> _initDatabase() async {
+  Future<void> _initUserSettingsDatabase() async {
+    final databasePath = join(await getDatabasesPath(), 'user_settings.db');
+
+    _userSettingsDatabase = await openDatabase(
+      databasePath,
+      onCreate: (db, version) {
+        return db.execute(
+          'CREATE TABLE user_settings(id INTEGER PRIMARY KEY, numColumns INTEGER)',
+        );
+      },
+      version: 1,
+    );
+
+    final List<Map<String, dynamic>> maps =
+        await _userSettingsDatabase.query('user_settings');
+    if (maps.isEmpty) {
+      await _userSettingsDatabase.insert(
+        'user_settings',
+        {'numColumns': 3},
+        conflictAlgorithm: ConflictAlgorithm.replace,
+      );
+    } else {
+      _numColumns = maps[0]['numColumns'];
+    }
+  }
+
+  Future<void> _initMoviesDatabase() async {
     final databasePath = join(await getDatabasesPath(), 'movie_log.db');
 
-    _database = await openDatabase(
+    _moviesDatabase = await openDatabase(
       databasePath,
       onCreate: (db, version) {
         return db.execute(
@@ -27,11 +55,11 @@ class MovieLogProvider with ChangeNotifier {
       },
       version: 1,
     );
-    await _loadMovies(_database);
+    await _loadMovies(_moviesDatabase);
   }
 
   Future<void> addMovieList(Movie movie) async {
-    await _database.insert(
+    await _moviesDatabase.insert(
       'movies',
       movie.toMap(),
       conflictAlgorithm: ConflictAlgorithm.replace,
@@ -55,6 +83,11 @@ class MovieLogProvider with ChangeNotifier {
 
   void changeNumColumns(int newNumColumns) {
     _numColumns = newNumColumns;
+    _userSettingsDatabase.update(
+      'user_settings',
+      {'numColumns': newNumColumns},
+      where: 'id = 1',
+    );
     notifyListeners();
   }
 }
